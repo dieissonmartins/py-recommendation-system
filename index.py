dc2fa5f43dda25718627eb13f242a99bcb988bb5
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-10-04 13:22:32 -0300

    get data (Books,Ratings,Users)

--- index.py
+++ index.py
@@ -1,16 +1,12 @@
-# work data
-import pandas as pd
-
-# work matrices
-import numpy as np
-
-# work graphics
-import matplotlib.pyplot as plt
+import warnings  # disable warnings
+import pandas as pd  # work data
+import matplotlib.pyplot as plt  # work graphics
+import numpy as np  # work matrices
 import seaborn as sns
 import plotly.express as px
+import os
 
-# disable warnings
-import warnings
+path = os.path.dirname(os.path.realpath(__file__))
 
 warnings.filterwarnings('ignore')
 
@@ -20,7 +16,19 @@ print('Script de recomendações')
 pd.set_option('display.max_rows', 100)
 pd.set_option('display.max_columns', 50)
 
-# configs matplotlib Z
+# configs matplotlib
 # size graphic
 plt.rcParams['figure.figsize'] = (15, 6)
 plt.style.use('seaborn-darkgrid')
+
+# get data
+path_books = path + '/tmp/Books.csv'
+data_books = pd.read_csv(path_books)
+
+# get ratings
+path_ratings = path + '/tmp/Ratings.csv'
+data_ratings = pd.read_csv(path_ratings)
+
+# get users
+path_users = path + '/tmp/Users.csv'
+data_users = pd.read_csv(path_users)
